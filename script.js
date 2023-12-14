7b47052db360660f89a5ec8976e73b8aac426f5d
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-12-14 16:13:58 +0100

    Add checkWin and restart function
    implement checkWin func to findout if any of the winningCombinations have turned true after the currPlayer chose a field;
    if no winner is found the function will check if there is a draw (gameboard full) so that the game would end, in case every field on the board has been picked;
    
    add restart function that will reset the game and start another round;
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -7,17 +7,54 @@
         gameboard: ["", "", "", "", "", "", "", "", ""]
     };
 
-    setField = function(field, currPlayer) {
+    setField = function(field) {
+        console.log(currPlayer);
         gameboard.gameboard[field] = currPlayer;
         console.log(gameboard);
-        changePlayer();
-        render();
+        checkWin();
+        console.log("checking for Win");
     };
 
     changePlayer = function() {
         currPlayer = (currPlayer === player_x) ? player_o : player_x
-        console.log(currPlayer);
+        console.log(`Player has been changed to ${currPlayer}`);
     };
+
+    checkWin = function() {
+        winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+     
+
+        win = winningCombinations.some(combination => {
+            return combination.every(index => gameboard.gameboard[index] === currPlayer)
+        });
+        draw = [0, 1, 2, 3, 4, 5, 6, 7, 8].every(index => gameboard.gameboard[index] !== "")
+
+        if (win) {
+            console.log(`${currPlayer} Won the Game!`);
+            restart();
+        } else if (draw) {
+            console.log("It's a draw!")
+            restart();
+        } else {
+            changePlayer();
+            return;
+        };
+    };
+
+    restart = function() {
+        gameboard.gameboard = ["", "", "", "", "", "", "", "", ""];
+    };
+
 })();
 
 
