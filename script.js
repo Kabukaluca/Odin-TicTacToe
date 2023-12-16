5497a4d8cc15e8dd1af3d734df9faa400a6cf806
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-12-16 14:59:07 +0100

    Turn spaghetti code into factory funcs
    turn spagetthi code into factories & clousures to prevent changes from the outside;
    provide better structured code;
    
    [Ticket: X]

--- script.js
+++ script.js
@@ -1,62 +1,67 @@
 (function() {
-    const player_x = "x"
-    const player_o = "o"
-    let currPlayer = player_x
+    player = {
+        player_x: "x",
+        player_o: "o",
+        currPlayer: "x",
+
+        switchPlayer: function() {
+            return (
+                this.currPlayer = (this.currPlayer === this.player_x) ? this.player_o : this.player_x,
+                console.log(`Player has been changed to ${this.currPlayer}`)
+            );
+        },
+    };
 
     gameboard = {
         gameboard: ["", "", "", "", "", "", "", "", ""]
     };
 
-    setField = function(field) {
-        console.log(currPlayer);
-        gameboard.gameboard[field] = currPlayer;
-        console.log(gameboard);
-        checkWin();
-        console.log("checking for Win");
+    gameLogic = {
+        setField(field) {
+            return (
+                console.log(player.currPlayer),
+                gameboard.gameboard[field] = player.currPlayer,
+                console.log(gameboard),
+                gameLogic.checkWin(),
+                console.log("checking for Win")
+            );
+        },
+
+        restart() {
+            return (
+                gameboard.gameboard = ["", "", "", "", "", "", "", "", ""]
+            )
+        },
+
+        checkWin() {
+            winningCombinations = [
+                [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], 
+                [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+            ];
+
+            win = winningCombinations.some(combination => {
+                return combination.every(index => gameboard.gameboard[index] === player.currPlayer);
+            });
+            draw = [0, 1, 2, 3, 4, 5, 6, 7, 8].every(index => gameboard.gameboard[index] !== "");
+
+            if (win) {
+                return(
+                    console.log(`${player.currPlayer} Won the Game!`),
+                    gameLogic.restart()
+                );
+            } else if (draw) {
+                return (
+                    console.log("It's a draw!"),
+                    gameLogic.restart()
+                );
+            } else {
+                return (
+                    player.switchPlayer()
+                );
+            };
+        },
     };
-
-    changePlayer = function() {
-        currPlayer = (currPlayer === player_x) ? player_o : player_x
-        console.log(`Player has been changed to ${currPlayer}`);
-    };
-
-    checkWin = function() {
-        winningCombinations = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-
-     
-
-        win = winningCombinations.some(combination => {
-            return combination.every(index => gameboard.gameboard[index] === currPlayer)
-        });
-        draw = [0, 1, 2, 3, 4, 5, 6, 7, 8].every(index => gameboard.gameboard[index] !== "")
-
-        if (win) {
-            console.log(`${currPlayer} Won the Game!`);
-            restart();
-        } else if (draw) {
-            console.log("It's a draw!")
-            restart();
-        } else {
-            changePlayer();
-            return;
-        };
-    };
-
-    restart = function() {
-        gameboard.gameboard = ["", "", "", "", "", "", "", "", ""];
-    };
-
 })();
-
 
 /* GET FIELDS ON CLICK
 const fields = document.querySelectorAll(".field");
@@ -69,4 +74,3 @@ fields.forEach(field => {
     })
 })
 */
-
